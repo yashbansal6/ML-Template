800647ec3122cf859ee4e7946c52f67e47e20d79
Yash Bansal <yashbansal@outlook.com>
2020-01-29 18:47:12 +0530

    Create folds in skf

---
+++ src/create_folds.py
@@ -0,0 +1,20 @@
+# to check distribution
+import pandas as pd
+from sklearn import model_selection
+
+if __name__ == "__main__":
+    df = pd.read_csv("input/train.csv")
+    df["kfold"] = -1
+    
+    # shuffle data and drop the original indices 
+    df = df.sample(frac=1).reset_index(drop=True)
+    
+    kf = model_selection.StratifiedKFold(n_splits=5, shuffle=False, random_state=None)
+    
+    # enumerate provides indices to be stored in fold
+    for fold, (train_idx, val_idx) in enumerate(kf.split(X=df, y=df.target.values)):
+        print(len(train_idx), len(val_idx))
+        df.loc[val_idx, 'kfold'] = fold
+    
+    #df.to_csv("input/train_folds.csv", index=False)
+    
